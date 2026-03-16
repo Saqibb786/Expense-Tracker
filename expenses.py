df52d7876b9f566bd2f126e22b7ddc8d51382a4a
Saqib Ali Butt <saqibbutt10000@gmail.com>
2026-03-16 16:26:50 +0500

    Tested Connection

---
+++ expenses.py
@@ -0,0 +1,7 @@
+def print_menu():
+    print('1. Add expense')
+    print('2. View all expenses')
+    print('3. View total by category')
+    print('4. View monthly summary')
+    print('5. Exit')
+    print('Enter choice (1-5): ')
---
+++ main.py
@@ -0,0 +1,9 @@
+import expenses
+
+
+def main():
+    expenses.print_menu()
+
+
+if __name__ == "__main__":
+    main()
